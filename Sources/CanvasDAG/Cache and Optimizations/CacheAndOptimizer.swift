4c9e1298ca3bfa6a104d85b03f0d5c743a7f510e
Greg Fajen <gregfajen@gmail.com>
2020-01-07 19:08:13 -0500

    optimization logging

--- Sources/CanvasDAG/Cache and Optimizations/CacheAndOptimizer.swift
+++ Sources/CanvasDAG/Cache and Optimizations/CacheAndOptimizer.swift
@@ -34,8 +34,8 @@ public class CacheAndOptimizer {
         var graph: Graph = graph
         var map1 = [NodeKey:NodeKey]()
 
-//        print("BEFORE:")
-//        graph.subgraph(for: subgraphKey).finalNode?.log()
+        print("BEFORE:")
+        graph.subgraph(for: subgraphKey).finalNode?.log()
 
         var added = Set<NodeKey>()
         graph = insertPreExistingCaches(graph, added: &added)
@@ -55,8 +55,8 @@ public class CacheAndOptimizer {
         graph = placeNewCaches(graph, map2, initial)
         pruneOldCaches()
 
-//        print("AFTER:")
-//        graph.subgraph(for: subgraphKey).finalNode?.log()
+        print("AFTER:")
+        graph.subgraph(for: subgraphKey).finalNode?.log()
         
 //        let e =  graph.subgraph(for: subgraphKey).finalNode?.calculatedRenderExtent ?? .nothing
 //        print("extent: \(e)")
